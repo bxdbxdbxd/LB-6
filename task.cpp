f86b1b89c6ff8d646622c7ffcdd3d228f303e187
darya.sam54@gmail.com <99540593+bxdbxdbxd@users.noreply.github.com>
2024-05-31 17:01:49 +0700

    task 3

---
+++ task.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int len_str = 5, len_col = 4;
+    const double matrix_val[4][5] = {{-0.77, -0.04, 0.21, -18, -1.24},
+                                {0.25, -1.23, 0.16, -0.09, 1.12},
+                                {-0.21, 0.16, 0.8, -0.13, 2.56},
+                                {0.15, -1.31, 0.06, 1.12, -0.77}};
+    
+    return 0;
+}
+
+void f(int start, int end) {
+    for (int i = start; i < end; i++) {
+        
+    }
+}
